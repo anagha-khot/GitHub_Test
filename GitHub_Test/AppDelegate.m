fba6741af04f948a0e29c6a6f2816a5dfdcde172
tejashri-pawar <anagha.khot@ninedots.com>
2016-02-23 10:23:07 +0530

    fork

--- GitHub_Test/AppDelegate.m
+++ GitHub_Test/AppDelegate.m
@@ -19,6 +19,7 @@
     // Override point for customization after application launch.
     NSLog(@"DID Finish called");
         NSLog(@"After Merge master");
+            NSLog(@"Fork master");
     return YES;
 }
 
