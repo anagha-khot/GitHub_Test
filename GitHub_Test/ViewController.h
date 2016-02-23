8fdc14236ffe4d098a645609034587cdaec0fe31
anagha-khot <anagha.khot@ninedots.com>
2016-02-23 09:45:20 +0530

    Initial Commit

---
+++ GitHub_Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHub_Test
+//
+//  Created by Team Sure on 23/02/16.
+//  Copyright © 2016 Ninedots. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitHub_Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitHub_Test
+//
+//  Created by Team Sure on 23/02/16.
+//  Copyright © 2016 Ninedots. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
